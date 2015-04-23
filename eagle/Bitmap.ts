43db457f05d821e2bfe888c99735f40fa776c094
chris.hsu <bingyuwuhen@gmail.com>
2015-04-23 10:38:46 +0800

    bitmap修改

--- eagle/Bitmap.ts
+++ eagle/Bitmap.ts
@@ -9,6 +9,8 @@ module eagle {
 
         private _useIrregularMask:boolean = false;
 
+        private _renderTexture:egret.RenderTexture;
+
         /**
          * 渲染纹理
          * @member {egret.Texture} egret.Bitmap#texture
@@ -18,14 +20,14 @@ module eagle {
         }
 
         public set texture(value:egret.Texture) {
-            if (value == this["_texture"]) {
+            if (this._bitmap && this._bitmap.texture == value) {
                 return;
             }
-            this._setSizeDirty();
-            this["_texture"] = value;
             if (this._useIrregularMask) {
                 this._bitmap.texture = value;
                 this.updateMask();
+            } else {
+                this["_texture"] = value;
             }
         }
 
@@ -35,15 +37,14 @@ module eagle {
         public updateMask() {
             // 不规则遮罩，使用 RenderTexture + BlendMode 实现，这个调用方式在未来会进一步简化
             // 实现原理： 创建一个 Container，在其中先添加一个图片，然后再添加一个蒙版，并将蒙版的混合模式设置为擦除，然后将这个 Container 通过 RenderTexture 绘制为一个纹理
-            var texture = new egret.RenderTexture();
             //为保证擦除结果正确，传入clipRect参数，规定最终RenderTexture大小只有mask区域大小
-            texture.drawToTexture(this._container, new egret.Rectangle(
+            this._renderTexture.drawToTexture(this._container, new egret.Rectangle(
                 this._irregularMask.x,
                 this._irregularMask.y,
                 this._irregularMask.width,
                 this._irregularMask.height));
             this._setSizeDirty();
-            this["_texture"] = texture;
+            egret.Logger.info("updateMask");
         }
 
         /**
@@ -59,6 +60,8 @@ module eagle {
                 this._container.addChild(this._bitmap);
                 value.blendMode = egret.BlendMode.ERASE_REVERSE;
                 this._container.addChild(value);
+                this._renderTexture = new egret.RenderTexture();
+                this["_texture"] = this._renderTexture;
             } else {
                 this._container.removeChild(this._irregularMask);
                 this._container.addChild(value);
