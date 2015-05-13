fc0025cedb85b3e08c526b2e3749aa90f8e3e49c
chris.hsu <bingyuwuhen@gmail.com>
2015-05-13 12:19:43 +0800

    注释说明

--- eagle/JSONPLoader.ts
+++ eagle/JSONPLoader.ts
@@ -1,5 +1,13 @@
 module eagle {
 
+    /**
+     * @class eagle.JSONPLoader
+     * @classdesc
+     * JSONPLoader 类以文本形式从 URL 下载数据。
+     * JSONPLoader 对象会先从 URL 中下载所有数据，然后才将数据用于应用程序中的代码。它会发出有关下载进度的通知，
+     * @extends egret.EventDispatcher
+     * @link https://github.com/chrisbing/egret-eagle
+     */
     export class JSONPLoader extends egret.URLLoader {
 
         private static _regID:number = 0;
@@ -13,6 +21,13 @@ module eagle {
             this.callbackName = callbackName;
         }
 
+        /**
+         * 从指定的 URL 发送和加载数据。以文本格式接收数据。
+         * 请注意 使用JSONP加载将忽略 dataFormat 属性。
+         * 如果想将数据发送至指定的 URL，则可以在 URLRequest 对象中设置 data 属性，data属性应为{egret.URLVariables}或data.toString返回值符合URL查询字符串格式。
+         * @method egret.URLLoader#load
+         * @param request {URLRequest}  一个 URLRequest 对象，指定要下载的 URL。
+         */
         load(request:egret.URLRequest):void {
 
             // 保证 回调的id是最初生成的id
